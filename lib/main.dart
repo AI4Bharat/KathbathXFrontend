a3242462d1278d12c71bfcf1d0dac4901a4924a2
Gree606 <sleya7110@gmail.com>
2025-03-06 14:03:40 +0530

    added safe are inside the scafforld for the pages

--- lib/main.dart
+++ lib/main.dart
@@ -31,44 +31,44 @@ import 'package:android_play_install_referrer/android_play_install_referrer.dart
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   // initialize firebase crashlytics
-  try {
-    await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,
-    );
-    FlutterError.onError = (FlutterErrorDetails details) {
-      FlutterError.dumpErrorToConsole(details);
-      FirebaseCrashlytics.instance.recordFlutterFatalError(details);
-    };
-    PlatformDispatcher.instance.onError = (error, stack) {
-      print("The error is $error");
-      FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
-      return true;
-    };
-    await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
-    // Initialize the database
-    final db = await _openDatabase();
-    await dotenv.load(fileName: ".env");
+  // try {
+  //   await Firebase.initializeApp(
+  //     options: DefaultFirebaseOptions.currentPlatform,
+  //   );
+  //   FlutterError.onError = (FlutterErrorDetails details) {
+  //     FlutterError.dumpErrorToConsole(details);
+  //     FirebaseCrashlytics.instance.recordFlutterFatalError(details);
+  //   };
+  //   PlatformDispatcher.instance.onError = (error, stack) {
+  //     print("The error is $error");
+  //     FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+  //     return true;
+  //   };
+  //   await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
+  // Initialize the database
+  final db = await _openDatabase();
+  await dotenv.load(fileName: ".env");
 
-    // Check for in App update
-    if (Platform.isAndroid) {
-      await checkForAndroidUpdate();
-    } else if (Platform.isIOS) {
-      await checkForIOSUpdate();
-    }
-
-    SystemChrome.setPreferredOrientations([
-      DeviceOrientation.portraitUp,
-    ]).then((_) {
-      runApp(
-        ChangeNotifierProvider(
-          create: (context) => RecorderPlayerProvider(),
-          child: KaryaApp(db),
-        ),
-      );
-    });
-  } catch (e, stack) {
-    FirebaseCrashlytics.instance.recordError(e, stack, fatal: true);
+  // Check for in App update
+  if (Platform.isAndroid) {
+    await checkForAndroidUpdate();
+  } else if (Platform.isIOS) {
+    await checkForIOSUpdate();
   }
+
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]).then((_) {
+    runApp(
+      ChangeNotifierProvider(
+        create: (context) => RecorderPlayerProvider(),
+        child: KaryaApp(db),
+      ),
+    );
+  });
+  // } catch (e, stack) {
+  //   FirebaseCrashlytics.instance.recordError(e, stack, fatal: true);
+  // }
 }
 
 Future<void> checkForAndroidUpdate() async {
@@ -205,76 +205,92 @@ class _KaryaAppState extends State<KaryaApp> {
                         fontFamily: 'CustomFont'),
                     initialRoute: snapshot.data,
                     routes: {
-                      '/': (context) => LoginScreen(),
-                      '/register': (context) => const RegisterScreen(),
-                      '/dashboard': (context) => DashboardScreen(
-                          title: 'Dashboard Screen', db: widget.db),
+                      '/': (context) =>
+                          const Scaffold(body: SafeArea(child: LoginScreen())),
+                      '/register': (context) => const Scaffold(
+                          body: SafeArea(child: RegisterScreen())),
+                      '/dashboard': (context) => Scaffold(
+                          body: SafeArea(
+                              child: DashboardScreen(
+                                  title: 'Dashboard Screen', db: widget.db))),
                       '/sd_microtask': (context) {
                         final args = ModalRoute.of(context)!.settings.arguments
                             as Map<String, dynamic>;
-                        return SpeechRecordingScreen(
+                        return Scaffold(
+                            body: SafeArea(
+                                child: SpeechRecordingScreen(
                           db: widget.db,
                           microtasks:
                               args['microtasks'] as List<MicroTaskRecord>,
                           microtaskAssignments: args['microtaskAssignments']
                               as List<MicroTaskAssignmentRecord>,
-                        );
+                        )));
                       },
                       '/image_transcription_microtask': (context) {
                         final args = ModalRoute.of(context)!.settings.arguments
                             as Map<String, dynamic>;
-                        return ImageTranscriptionScreen(
+                        return Scaffold(
+                            body: SafeArea(
+                                child: ImageTranscriptionScreen(
                           db: widget.db,
                           microtasks:
                               args['microtasks'] as List<MicroTaskRecord>,
                           microtaskAssignments: args['microtaskAssignments']
                               as List<MicroTaskAssignmentRecord>,
-                        );
+                        )));
                       },
                       '/image_audio_microtask': (context) {
                         final args = ModalRoute.of(context)!.settings.arguments
                             as Map<String, dynamic>;
-                        return ImageAudioScreen(
+                        return Scaffold(
+                            body: SafeArea(
+                                child: ImageAudioScreen(
                           db: widget.db,
                           microtasks:
                               args['microtasks'] as List<MicroTaskRecord>,
                           microtaskAssignments: args['microtaskAssignments']
                               as List<MicroTaskAssignmentRecord>,
-                        );
+                        )));
                       },
                       '/speech_verification_microtask': (context) {
                         final args = ModalRoute.of(context)!.settings.arguments
                             as Map<String, dynamic>;
-                        return SpeechVerificationScreen(
+                        return Scaffold(
+                            body: SafeArea(
+                                child: SpeechVerificationScreen(
                           db: widget.db,
                           microtasks:
                               args['microtasks'] as List<MicroTaskRecord>,
                           microtaskAssignments: args['microtaskAssignments']
                               as List<MicroTaskAssignmentRecord>,
                           taskName: args['taskName'],
-                        );
+                        )));
                       },
                       '/speech_audio_refinement': (context) {
                         final args = ModalRoute.of(context)!.settings.arguments
                             as Map<String, dynamic>;
-                        return SpeechAudioScreen(
+                        return Scaffold(
+                            body: SafeArea(
+                                child: SpeechAudioScreen(
                           db: widget.db,
                           microtasks:
                               args['microtasks'] as List<MicroTaskRecord>,
                           microtaskAssignments: args['microtaskAssignments']
                               as List<MicroTaskAssignmentRecord>,
-                        );
+                        )));
                       },
                       '/video_collection_task': (context) {
                         final args = ModalRoute.of(context)!.settings.arguments
                             as Map<String, dynamic>;
-                        return VideoCollectionScreen(
+                        return Scaffold(
+                            body: SafeArea(
+                                child: VideoCollectionScreen(
                           db: widget.db,
                           microtasks:
                               args['microtasks'] as List<MicroTaskRecord>,
                           microtaskAssignments: args['microtaskAssignments']
                               as List<MicroTaskAssignmentRecord>,
-                        );
+                        )));
                       }
                     },
                   )));
